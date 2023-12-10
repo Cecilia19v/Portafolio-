36de287de9092799d143b663a9ee975375e1c0ce
Cecilia19v <151890739+Cecilia19v@users.noreply.github.com>
2023-12-09 23:12:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+ cursor: "<span style='color: #fb4993;'>|</span>",
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color: #fb4993;">Hago programación web y trabajo con temas ambientales.</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
